e2783ed4ddc05cde147b157a6a278d282f949a37
Rick Strahl <rstrahl@west-wind.com>
2014-03-18 01:47:18 -1000

    Update distribution

--- Distribution/PullFiles.bat
+++ Distribution/PullFiles.bat
@@ -1,5 +1,6 @@
 copy ..\Kuhela\bin\release\Kuhela.exe
 copy ..\Kuhela\bin\release\Kuhela.Core.dll
 copy ..\Kuhela\bin\release\FiddlerCore4.dll
+copy ..\Kuhela\bin\release\Newtonsoft.json.dll
 copy ..\Kuhela\bin\release\Westwind.Utilities.dll
 pause
