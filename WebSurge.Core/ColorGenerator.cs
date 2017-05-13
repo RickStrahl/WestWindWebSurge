6563e61afc21903bff5b7286c511bf1aa07aa695
Michael Sampatakakis <micksab.ms@gmail.com>
2017-05-13 19:57:04 -0400

    Minor modification of the color range..

--- WebSurge.Core/ColorGenerator.cs
+++ WebSurge.Core/ColorGenerator.cs
@@ -9,23 +9,29 @@ using System.Threading.Tasks;
 namespace WebSurge
 {
     /// <summary>
-    /// Class that is able to generate up to 56 visually distinct colors for use in graphs.
-    /// Based on information found at http://stackoverflow.com/questions/309149/generate-distinctly-different-rgb-colors-in-graphs.
-    /// Since the possibility of hosting more than 16 series in a single graph is highly unlikely, a hard-coded approach was selected.
+    /// Class that is able to generate up to 10 visually distinct colors for use in graphs.
+    /// Based on information found at http://tools.medialab.sciences-po.fr/iwanthue/
+    /// Since the possibility of hosting more than 10 series in a single graph is highly unlikely, a hard-coded approach was selected.
     /// </summary>
     public class ColorGenerator
     {
         private int _intCounter = 0;
         private static Color[] _colorValues = new Color[] {
-       Color.Red, Color.Green, Color.Blue, Color.Yellow,
-       Color.Pink, Color.DarkSeaGreen, Color.Turquoise, Color.Gold,
-       Color.Magenta, Color.SpringGreen, Color.PowderBlue, Color.Khaki,
-       Color.Orange, Color.Olive, Color.Cyan, Color.Salmon
-    };
+            Color.FromArgb(255,104,42,0),
+            Color.FromArgb(255,1,255,153),
+            Color.FromArgb(255,255,108,208),
+            Color.FromArgb(255,0,108,45),
+            Color.FromArgb(255,223,0,19),
+            Color.FromArgb(255,192,255,209),
+            Color.FromArgb(255,0,25,83),
+            Color.FromArgb(255,222,132,0),
+            Color.FromArgb(255,175,191,255),
+            Color.FromArgb(255,101,0,59)
+        };
 
         /// <summary>
         /// Gets the next color out of the 16 available for use is graphs.
-        /// If all 16 have already been served, the sequence of colors is reset and served once more.
+        /// If all 10 have already been served, the sequence of colors is reset and served once more.
         /// </summary>
         /// <returns>A color value</returns>
         public Color GetNextColor()
