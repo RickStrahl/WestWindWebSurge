4699fff3118e4ba0217b5bb18a7ad05fd1c0a2cd
Rick Strahl <rstrahl@west-wind.com>
2018-05-06 00:04:40 -0700

    Update packages to latest versions and build release for v1.9

--- Install/build.ps1
+++ Install/build.ps1
@@ -38,4 +38,34 @@ If (Test-Path $AppData\html\_results.html) {
 .\signtool.exe sign /v /n "West Wind Technologies"  /sm /s MY /tr "http://timestamp.digicert.com" /td SHA256 /fd SHA256 "$release\WebSurgeSetup.exe"
 
 
-7z a -tzip "$release\WebSurgeSetup.zip" "$release\WebSurgeSetup.exe"
+.\7z a -tzip "$release\WebSurgeSetup.zip" "$release\WebSurgeSetup.exe"
+
+
+$version = [System.Diagnostics.FileVersionInfo]::GetVersionInfo("$PSScriptRoot\builds\currentrelease\WebSurgeSetup.exe").FileVersion
+$version = $version.Trim()
+"Initial Version: " + $version
+
+# Remove last two .0 version tuples if it's 0
+if($version.EndsWith(".0.0")) {
+    $version = $version.SubString(0,$version.Length - 4);
+}
+else {
+    if($version.EndsWith(".0")) {    
+        $version = $version.SubString(0,$version.Length - 2);
+    }
+}
+"Truncated Version: " + $version
+
+"Writing Version File for: " + $version
+$versionFilePath = ".\builds\currentrelease\WebSurge_Version_Template.xml"
+$versionFile = Get-Content -Path $versionFilePath  
+
+$versionFile = $versionFile.Replace("{{version}}",$version).Replace("{{date}}",[System.DateTime]::Now.ToString("MMMM d, yyyy"))
+$versionFile
+""
+
+out-file -filepath $versionFilePath.Replace("_Template","")  -inputobject $versionFile
+
+get-childitem .\builds\CurrentRelease\* -include *.* | foreach-object { "{0}`t{1}`t{2:n0}`t`t{3}" -f $_.Name, $_.LastWriteTime, $_.Length, [System.Diagnostics.FileVersionInfo]::GetVersionInfo($_).FileVersion }
+""
+"Done..."
