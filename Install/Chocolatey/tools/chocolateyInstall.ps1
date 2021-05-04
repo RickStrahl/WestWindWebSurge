42f4293b7368c011fff593cc38a6da35fc7f67a5
Rick Strahl <rstrahl@west-wind.com>
2021-05-03 16:35:40 -1000

    v1.22

--- Install/Chocolatey/tools/chocolateyInstall.ps1
+++ Install/Chocolatey/tools/chocolateyInstall.ps1
@@ -1,8 +1,8 @@
 $packageName = 'westwindwebsurge'
 $fileType = 'exe'
-$url = 'https://github.com/RickStrahl/WestwindWebSurgeReleases/raw/master/WebSurgeSetup-1.20.exe'
+$url = 'https://github.com/RickStrahl/WestwindWebSurgeReleases/raw/master/WebSurgeSetup-1.22.exe'
 
 $silentArgs = '/SILENT'
 $validExitCodes = @(0)
 
-Install-ChocolateyPackage "packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "5564636294BADFCDC917A08D84A0EA9C42D1036010A3CEC54CE6F307683C27EC" -checksumType "sha256"
+Install-ChocolateyPackage "packageName" "$fileType" "$silentArgs" "$url"  -validExitCodes  $validExitCodes  -checksum "C9F3601196F3BE788ED3937F1B69C3A6B71414E0B2B10A47561B0AB7099D5D74" -checksumType "sha256"
